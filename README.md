8fd7355030141c5556a28ced41107a8eccf68148
lijun <li.jun@demaotang.com>
2023-06-30 16:53:55 +0800

    添加mysql模块

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # thindb
-通用数据库连接和操作组件，如mysql、redis等
+
+基于 Nodejs 的通用数据库访问和操作组件，如 mysql、redis 等
+
+# Install
+
+```console
+npm install thindb
+```
+
+# Usage
+
+实例化引用
+
+```console
+const Thindb = require("thindb");
+const thindb = new Thindb([...config]);
+
+```
+
+## mysql
+
+```console
+let ddd=thindb.mysql.ddd();
+ddd.exePromise({sp:"",token:"",data:""});
+或者
+global.ddd_mysql.exePromise({sp:"",token:"",data:""});
+
+```
---
+++ thindb.js
@@ -0,0 +1,70 @@
+const db_mysql = require("mysql2");
+
+class thindb {
+	/**
+	 * 数据库链接配置
+	 * @param {Array} connections 数据库连接配置列表
+	 */
+	constructor(connections) {
+		this.connections = connections || [];
+	}
+
+	mysql = {
+		DEFAULT_CONFIGS: { multipleStatements: true },
+		DEFAULT_CMD: "select ? into @jdata;call ??(?,@jdata);select @jdata as jdata;",
+		connect: () => {
+			if (!global.ddd_mysql_pool) global.ddd_mysql_pool = {};
+			let mysqlConfigs = this.__getDbConfig(this.dbTypes.mysql);
+			mysqlConfigs?.forEach((cfg) => {
+				let connName = cfg.database;
+				if (!global.ddd_mysql_pool[connName]) global.ddd_mysql_pool[connName] = db_mysql.createPool({ ...this.mysql.DEFAULT_CONFIGS, ...cfg });
+			});
+		},
+		ddd: (poolName) => {
+			let ddd_mysql = {
+				exePromise: (p, dddMode = true) => {
+					!global.ddd_mysql_pool && this.mysql.connect();
+					poolName ||= Object.getOwnPropertyNames(global.ddd_mysql_pool)[0];
+					let pool = global.ddd_mysql_pool[poolName];
+
+					return new Promise((resolve, reject) => {
+						let cmd = dddMode ? this.mysql.DEFAULT_CMD : p.sp,
+							data = dddMode ? [JSON.stringify(p.data), p.sp, JSON.stringify(p.token)] : p.data;
+						pool.query(cmd, data, (err, rows, fields) => {
+							if (err) reject({ err_code: err.errno, err_message: err.sqlMessage });
+							else {
+								if (dddMode) {
+									let res = rows.pop();
+									resolve(res[0]?.jdata || {});
+								} else {
+									resolve(rows);
+								}
+							}
+						});
+					});
+				}
+			};
+			global.ddd_mysql ??= ddd_mysql;
+			return ddd_mysql;
+		}
+	};
+
+	redis() {}
+
+	__getDbConfig(dbType) {
+		let dbConfig = [];
+		Array.isArray(this.connections) &&
+			this.connections.forEach((conn) => {
+				let _dbType = Object.keys(conn)[0]?.toLowerCase();
+				_dbType === dbType && dbConfig.push(conn[_dbType]);
+			});
+		return dbConfig;
+	}
+
+	dbTypes = {
+		mysql: "mysql",
+		redis: "redis"
+	};
+}
+
+module.exports = thindb;
